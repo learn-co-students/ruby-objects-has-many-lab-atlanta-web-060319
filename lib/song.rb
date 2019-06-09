7a8acbd3ebb241becbd1726ce0241bb83d80358d
iheartobi <iobidah9@gmail.com>
2019-06-09 10:33:12 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+class Song
+    @@all = []
+    attr_accessor :artist, :name
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+    
+    def self.all
+        @@all
+    end
+
+    def artist_name
+        self.artist.name if artist
+    end
+
+end
